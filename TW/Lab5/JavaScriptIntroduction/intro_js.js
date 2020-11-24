a1cdecee9e9b66484d9722fc9fa99f4c2ed6bfc4
LeoCatarro <leonardocatarro@gmail.com>
2020-11-24 10:13:18 +0000

    TW Lab5

---
+++ TW/Lab5/JavaScriptIntroduction/intro_js.js
@@ -0,0 +1,16 @@
+var friends = ["Miguel" , "Joao" , "Leonardo" , "Diogo"];
+
+alert(friends[2]);
+
+var name = "Leonardo";
+
+if(name == "João" || name == "Maria")
+    alert("Esse tambem é o meu nome!");
+
+
+var myVariable = Math.floor(Math.random() * 10) + 1;
+
+if(myVariable > 5)
+    alert("upper");
+else
+    alert("lower");
