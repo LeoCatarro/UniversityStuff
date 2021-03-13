32fa7f581368fc708359b9821e30b4a5cce87e36
LeoCatarro <leonardocatarro@gmail.com>
2021-03-13 21:14:50 +0000

    Eda1-ficha1-ex3

--- EDA1/2-tentativa/ficha1/ex3.c
+++ EDA1/2-tentativa/ficha1/ex3.c
@@ -1,14 +1,82 @@
 #include <stdio.h>
 
 void ordena(int *a, int *b, int *c)
-{
-    printf("A: %d | B: %d | C: %d", *a, *b, *c);
-}
+{   
+    
+    int max = *a;       //Consider a as max!
+    int temp;
+
+    //Check Max
+    if(*b > max)
+        max = *b;
+    
+    if(*c > max)
+        max = *c;
+    
+
+    //Check others
+    if(max == *a)
+    {
+        //A > C > B
+        if(*b < *c) 
+        {
+            temp = *b;
+            *b = *c;
+            *c = temp;
+        }
+
+        //A > B >C
+        if(*b > *c)
+            //Ordenado logo de inicio!
+            return;
+    }
+
+    if(max == *b)
+    {
+        if(*a > *c)
+        {
+            temp = *a;
+            *a = *b;
+            *b = temp;
+        }
+
+        if(*a < *c)
+        {
+            temp = *a;
+            *a = *b;
+            *b = *c;
+            *c = temp;
+        }
+    }
+
+    if(max == *c)
+    {
+        if(*a > *b)
+        {
+            temp = *a;
+            *a = *c;
+            *c = *b;
+            *b = temp;
+        }
+
+        //*a < *b
+        if(*a < *b)
+        {
+            temp = *a;
+            *a = *c;
+            *c = temp;
+
+        }
+    }
+}   
 
 int main() 
 {
     int n1, n2, n3;
     scanf("%d %d %d", &n1, &n2, &n3);
     ordena(&n1, &n2, &n3);
+
+    printf("A: %d | B: %d | C: %d", n1, n2, n3);
+
     return 0;
 }
