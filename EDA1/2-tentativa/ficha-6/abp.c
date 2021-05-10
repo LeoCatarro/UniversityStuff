52bbf5a394117001cb97ff89d484105b7adbb31f
leocatarro <leonardocatarro@gmail.com>
2021-05-10 16:29:50 +0100

    [EDA1]: Just left Delete() && Insert() functions

--- EDA1/2-tentativa/ficha-6/abp.c
+++ EDA1/2-tentativa/ficha-6/abp.c
@@ -8,19 +8,61 @@ struct TreeNode{
     SearchTree  Right;
 };
 
+
+SearchTree CreateTree(ElementType rootElement)
+{
+    SearchTree T = malloc(sizeof(struct TreeNode));
+    T->Element = rootElement;
+    T->Left = NULL;
+    T->Right = NULL;
+    return T;
+}
+
+
 SearchTree MakeEmpty( SearchTree T ){
+    if( T != NULL ){
+        MakeEmpty( T->Left );
+        MakeEmpty( T->Right );
+        free( T );
+    }
+    return NULL; 
 }
 
 
 Position Find( ElementType X, SearchTree T ){
+    if( T == NULL )
+        return NULL;
+
+    if( X < T->Element )
+        return Find( X, T->Left );
+
+    else
+        if( X > T->Element )
+            return Find( X, T->Right );
+        else
+            return T;
 }
 
 
 Position FindMin( SearchTree T ){
+    if( T == NULL )
+        return NULL;
+    else
+        if( T->Left == NULL )
+            return T;
+        else
+            return FindMin(T->Left); 
 }
 
 
 Position FindMax( SearchTree T ){
+    if( T == NULL )
+        return NULL;
+    else
+        if( T->Right == NULL )
+            return T;
+        else
+            return FindMax( T->Right ); 
 }
 
 
@@ -33,4 +75,12 @@ SearchTree Delete( ElementType X, SearchTree T ){
 
 
 ElementType Retrieve( Position P ){
+    return P->Element;
+}
+
+
+int main()
+{
+
+    return 0;
 }
